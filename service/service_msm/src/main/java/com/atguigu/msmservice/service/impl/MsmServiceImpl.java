ccfa7f5b0de644c0ec9b3320bc67c51304ca7858
zpw1234 <55697433+zpw1234@users.noreply.github.com>
2020-10-19 16:08:48 +0800

    Update MsmServiceImpl.java

--- service/service_msm/src/main/java/com/atguigu/msmservice/service/impl/MsmServiceImpl.java
+++ service/service_msm/src/main/java/com/atguigu/msmservice/service/impl/MsmServiceImpl.java
@@ -21,7 +21,7 @@ public class MsmServiceImpl implements MsmService {
         if(StringUtils.isEmpty(phone)) return false;
 
         DefaultProfile profile =
-                DefaultProfile.getProfile("default", "LTAI4G9ynKXThNT4fqBexPDv", "4qVxYHeaqR7j68mfqlcN8dX2xhfoKg");
+                DefaultProfile.getProfile("default", "", "");
         IAcsClient client = new DefaultAcsClient(profile);
 
         //设置相关参数
